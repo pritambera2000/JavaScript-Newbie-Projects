bb63085e7c8972c125e374b90e5f7c2ce77c560e
Pritam Bera <photography.pritambera@gmail.com>
2021-10-12 03:01:55 +0530

    Added 05-ImageSlider(pc-beta)

--- 05-ImageSlider/JavaScript/script.js
+++ 05-ImageSlider/JavaScript/script.js
@@ -1,17 +1,40 @@
 const imgs = document.getElementById("images");
 
 const img  = document.querySelectorAll("#images img");
+const btn = document.querySelectorAll("img")
+
+
  let i =0
 function slide(){
    i++
     if ( i > img.length -1) {
         i = 0
     }
-    imgs.style.transform = `translateX(${(-i * 400)}px)`
+        imgs.style.transform = `translateX(${(-i * 400)}px)`
+    
+    
 
     // setTimeout(slide, 1000);
 }
 
+
+
 // slide();
 // console.log(pb)
 setInterval(slide, 3000);
+
+// imgs.addEventListener('click',()=>{
+//     let i=0;
+//     i++
+//     // if(i>imgs.length -1){
+//     //     i=0
+//     // }
+//     for (let i = 0; i > imgs.length -1; i++) {
+//     //  const element = array[i];
+//     if(i>imgs.length -1){
+//         i=0
+//     }
+//        imgs.style.transform = `translateX(${(-i*400)}px)`
+//     }
+  
+// })
