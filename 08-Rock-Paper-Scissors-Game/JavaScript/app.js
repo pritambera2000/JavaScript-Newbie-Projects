8b75d8ee737b45341e50522bca5b75c6c3a70818
Pritam Bera <pritambera440@gmail.com>
2022-03-13 22:48:32 +0530

    Finished Game Functionality But unfinished Design

--- 08-Rock-Paper-Scissors-Game/JavaScript/app.js
+++ 08-Rock-Paper-Scissors-Game/JavaScript/app.js
@@ -1,52 +1,77 @@
-/*
-const buttons = document.querySelectorAll(".hand");
-const scoreEl = document.getElementById('score');
-const choices = ["rock", "paper", "scissors"];
+let Score = 0;
 
-// console.log(randomChoice());
+const pickPlayersChoice = hand => {
+  // Hiding the triangle
 
-let userChoice;
-let score = 0;
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    userChoice = button.getAttribute("data-choice");
-    console.log(userChoice);
-    
-    checkWinner();
-  });
-});
-function checkWinner() {
-    const computerChoice = randomChoice();
-    console.log(computerChoice);
-    
-    if (userChoice === computerChoice) {
-      //user draw
-    } else if (
-      (userChoice === "Paper" && computerChoice === "rock") ||
-      (userChoice === "Scissors" && computerChoice === "paper") ||
-      (userChoice === "Rock" && computerChoice === "scissors")
-    ) {
-      //user won
-      updateScore(1);
-    } else {
-      // user lost
-      updateScore(-1);
-    }
-  }
-  function updateScore(value){
-      score += value
-      scoreEl.innerText = score;
-  }
-
-function randomChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-*/
-
-const pickPlayersChoice = (hand) => {
-  console.log(hand);
-  let gameBoard = document.querySelector('.game-board');
+  let gameBoard = document.querySelector(".game-board");
   gameBoard.style.display = "none";
- 
-  
-}
+
+  // SHowing the contest section
+  let contest = document.querySelector(".contest");
+  contest.style.display = "grid";
+
+  // User Picked Hand
+  if (hand == "rock") {
+    document.getElementById("userPickedHand").src = "assets/images/Rock.png";
+  } else if (hand == "paper") {
+    document.getElementById("userPickedHand").src = "assets/images/Paper.png";
+  } else if (hand == "scissors") {
+    document.getElementById("userPickedHand").src =
+      "assets/images/Scissors.png";
+  }
+
+  let compHand = pickComputerHand();
+  reffere(hand, compHand);
+};
+//Picking Computer Hand
+const pickComputerHand = () => {
+  let hands = ["rock", "paper", "scissors"];
+  let compHand = hands[Math.floor(Math.random() * 3)];
+
+  if (compHand == "rock") {
+    document.getElementById("computerPickedHand").src =
+      "assets/images/Rock.png";
+  } else if (compHand == "paper") {
+    document.getElementById("computerPickedHand").src =
+      "assets/images/Paper.png";
+  } else if (compHand == "scissors") {
+    document.getElementById("computerPickedHand").src =
+      "assets/images/Scissors.png";
+  }
+
+  return compHand;
+};
+// Making Decision For WIN LOOSE or TIE
+const reffere = (hand, compHand) => {
+  if (hand === compHand) {
+    setDecision("It's a Tie");
+  } else if (
+    (hand === "paper" && compHand === "rock") ||
+    (hand === "scissors" && compHand === "paper") ||
+    (hand === "rock" && compHand === "scissors")
+  ) {
+    setDecision("You Win!");
+    setScore(Score + 1);
+  } else {
+    setDecision("You Loose");
+  }
+};
+
+// WIN LOOSE or TIE
+const setDecision = decision => {
+  document.querySelector(".decision h1").innerText = decision;
+};
+//Setting the Score
+
+const setScore = score => {
+  Score = score;
+  document.getElementById("score").innerText = score;
+};
+//Function For Restarting the game on Play Again Button Click
+const restartGame = () => {
+  let gameBoard = document.querySelector(".game-board");
+  gameBoard.style.display = "flex";
+
+  let contest = document.querySelector(".contest");
+  contest.style.display = "none";
+};
